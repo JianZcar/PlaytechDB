e5f0f1820a332e0bfb08fb6bccaece4b2e833c7c
JianZcar <esteban.jianzcar@outlook.com>
2025-05-28 12:12:16 +0800

    col error fixed

--- init.sql
+++ init.sql
@@ -48,7 +48,7 @@ CREATE TABLE orders (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT,
   total_price DECIMAL(10,2),
-  status TINYINT UNSIGNED CHECK (mycol BETWEEN 0 AND 3), -- e.g., 'Pending', 'Paid', 'Shipped', 'Delivered'
+  status TINYINT UNSIGNED CHECK (status BETWEEN 0 AND 3), -- e.g., 'Pending', 'Paid', 'Shipped', 'Delivered'
   order_date DATETIME,
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
@@ -77,7 +77,7 @@ CREATE TABLE cart (
 CREATE TABLE inventory_logs (
   id INT AUTO_INCREMENT PRIMARY KEY,
   product_id INT,
-  action TINYINT UNSIGNED CHECK (mycol BETWEEN 0 AND 2), -- 'add', 'remove', 'adjust'
+  action TINYINT UNSIGNED CHECK (action BETWEEN 0 AND 2), -- 'add', 'remove', 'adjust'
   quantity INT,
   admin_id INT,
   date_logged DATETIME,
